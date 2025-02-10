a1d184738dee58b062e979227e8247a8cbe07aa8
Alvarez-Lucas <lucasalvarez@pm.me>
2025-02-10 14:34:24 -0500

    feat(nvim): update conform configuration
    Disable aync, add json formatter, add ps1 formatter.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,27 +1,29 @@
 return {
-  "stevearc/conform.nvim",
-  event = {
-    "BufWritePre",
-  },
-  dependencies = {
-    "williamboman/mason.nvim",
-	{ "zapling/mason-conform.nvim", opts = {} },
-  },
-  opts = {
-    format_after_save = {
-      -- These options will be passed to conform.format()
-      -- timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
-      python = { "isort", "black" },
-      -- You can customize some of the format options for the filetype (:help conform.format)
-      rust = { "rustfmt", lsp_format = "fallback" },
-      -- Conform will run the first available formatter
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      toml = { "taplo" },
-    },
-  },
+	"stevearc/conform.nvim",
+	event = {
+		"BufWritePre",
+	},
+	dependencies = {
+		"williamboman/mason.nvim",
+		{ "zapling/mason-conform.nvim", opts = {} },
+	},
+	opts = {
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			ps1 = { lsp_format = "prefer" },
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt", lsp_format = "fallback" },
+			-- Conform will run the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			-- toml = { "taplo" },
+			json = { "prettier" },
+		},
+	},
 }
