8ff1e1b594b8d584efb2e3902b9adb8c794a4ef6
Alvarez-Lucas <lucasalvarez@pm.me>
2025-04-25 17:31:54 -0400

    feat: update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2004,22 +2004,27 @@ require("lazy").setup({
 						"cd $dir &&",
 						"dotnet run",
 					},
-					c = function(...)
-						c_base = {
-							"cd $dir &&",
-							"gcc $fileName -o",
-							"/tmp/$fileNameWithoutExt",
-						}
-						local c_exec = {
-							"&& /tmp/$fileNameWithoutExt &&",
-							"rm /tmp/$fileNameWithoutExt",
-						}
-						vim.ui.input({ prompt = "Add more args:" }, function(input)
-							c_base[4] = input
-							vim.print(vim.tbl_extend("force", c_base, c_exec))
-							require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
-						end)
-					end,
+					c = {
+						"cd $dir &&",
+						"gcc $fileName -o $fileNameWithoutExt &&",
+						"$dir/$fileNameWithoutExt",
+					},
+					-- c = function(...)
+					-- 	c_base = {
+					-- 		"cd $dir &&",
+					-- 		"gcc $fileName -o",
+					-- 		"/tmp/$fileNameWithoutExt",
+					-- 	}
+					-- 	local c_exec = {
+					-- 		"&& /tmp/$fileNameWithoutExt &&",
+					-- 		"rm /tmp/$fileNameWithoutExt",
+					-- 	}
+					-- 	vim.ui.input({ prompt = "Add more args:" }, function(input)
+					-- 		c_base[4] = input
+					-- 		vim.print(vim.tbl_extend("force", c_base, c_exec))
+					-- 		require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+					-- 	end)
+					-- end,
 				},
 			},
 		},
